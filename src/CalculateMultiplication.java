5668effc3dbceeb661fd21c213acb6e7c46a9fce
Peter Ivanic <peterivanic89@gmail.com>
2022-09-09 11:52:04 +0200

    new infoMessage and isArgumentCorrect methods

--- src/CalculateMultiplication.java
+++ src/CalculateMultiplication.java
@@ -1,34 +1,9 @@
 public class CalculateMultiplication {
 
     public static void main(String[] args) {
-        if (args.length >= 1) {
-            if (args.length == 3 && isNumeric(args[1]) && isNumeric(args[2])) {
-                if (args[1].equals("-alg1") || args[1].equals("-alg2")) {
-                    switch (args[1]) {
-                        case "-alg1":
-                        case "-alg2":
-                    }
-                } else {
-                    badArgumentMessage();
-                }
-            } else {
-                badArgumentMessage();
-            }
-        } else {
-            System.out.println("Command Line Multiplication app\n" +
-                    "=============================");
-            System.out.println();
-            System.out.println("Command line arguments:");
-            System.out.println(
-                    """
-                                -alg1 BigIntegers multiply
-                                -alg2 Multiply algorithm
-                                
-                                input must be: -alg1/-alg2 first_number second_number
-                                
-                            """);
-        }
+
     }
+
     public static boolean isNumeric(String str) {
         int sum = 0;
         for (char ch : str.toCharArray())
@@ -49,5 +24,39 @@ public class CalculateMultiplication {
                             input must be: -alg1/-alg2 first_number second_number
                             
                         """);
+    }
+
+    public static void infoMessage() {
+        System.out.println("Command Line Multiplication app\n" +
+                "=============================");
+        System.out.println();
+        System.out.println("Command line arguments:");
+        System.out.println(
+                """
+                            -alg1 BigIntegers multiply
+                            -alg2 Multiply algorithm
+                            
+                            input must be: -alg1/-alg2 first_number second_number
+                            
+                        """);
+    }
+
+    public static boolean isArgumentsCorrect(String[] args){
+        if (args.length >= 1) {
+            if (args.length == 3 && isNumeric(args[1]) && isNumeric(args[2])) {
+                if (args[1].equals("-alg1") || args[1].equals("-alg2")) {
+                return true;
+                } else {
+                    badArgumentMessage();
+                    return false;
+                }
+            } else {
+                badArgumentMessage();
+                return false;
+            }
+        } else {
+            infoMessage();
+            return false;
+        }
     }
 }
